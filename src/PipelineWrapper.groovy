5fa7227f510c8c36cac0bc15102332a2472cab38
debian-jenkins <git@segebrecht.org>
2017-04-30 21:15:41 +0200

    first

---
+++ src/PipelineWrapper.groovy
@@ -0,0 +1,27 @@
+#!groovy
+
+/**
+ * Created by debian-jenkins on 30.04.17.
+ */
+class Pipeline {
+
+    def _this
+
+    Pipeline(def __this) {
+        _this=__this
+    }
+
+    def echo(def line) {
+        _this.echo(line)
+    }
+
+    def stage(def name, def closure) {
+        _this.stage(closure(name))
+    }
+}
+
+def call() {
+    return new Pipeline(this)
+}
+
+return this;
